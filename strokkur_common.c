0d5745c62de43070dd88ae2b2b6427b6a916c22a
Paul Khuong <pvk@pvk.ca>
2016-03-05 23:21:32 -0500

    strokkur_common: tune block_xor for gcc/sandybridge+

--- strokkur_common.c
+++ strokkur_common.c
@@ -13,18 +13,30 @@ xor_cache_line(void *restrict acc, const void *restrict src)
         uint8_t *acc8 = acc;
         const uint8_t *src8 = src;
 
-        for (size_t j = 0; j < 4; j++) {
-                vacc[j] = _mm_loadu_si128((void *)&acc8[j * 16]);
-                vsrc[j] = _mm_loadu_si128((const void *)&src8[j * 16]);
-        }
+#define LOAD(I) do {                                                    \
+                vacc[I] = _mm_loadu_si128((void *)&acc8[I * 16]);       \
+                vsrc[I] = _mm_loadu_si128((const void *)&src8[I * 16]); \
+        } while (0)
 
-        for (size_t j = 0; j < 4; j++) {
-                vacc[j] = _mm_xor_si128(vacc[j], vsrc[j]);
-        }
+        LOAD(0);
+        LOAD(1);
+        LOAD(2);
+        LOAD(3);
+#undef LOAD
 
-        for (size_t j = 0; j < 4; j++) {
-                _mm_storeu_si128((void *)&acc8[j * 16], vacc[j]);
-        }
+#define XOR(I) (vacc[I] = _mm_xor_si128(vacc[I], vsrc[I]))
+        XOR(0);
+        XOR(1);
+        XOR(2);
+        XOR(3);
+#undef XOR
+
+#define STORE(I) (_mm_storeu_si128((void *)&acc8[I * 16], vacc[I]))
+        STORE(0);
+        STORE(1);
+        STORE(2);
+        STORE(3);
+#undef STORE
 
         return;
 }
@@ -36,9 +48,19 @@ strokkur_block_xor(void *restrict acc, const void *restrict src, size_t n_bytes)
         const uint8_t *src8 = src;
         size_t i, round_bytes;
 
+        i = 0;
         round_bytes = n_bytes & ~63UL;
-        for (i = 0; i < round_bytes; i += 64) {
-                xor_cache_line(acc8 + i, src8 + i);
+        if (__builtin_expect(round_bytes > 0, 1)) {
+                for (; i < round_bytes; i += 64) {
+                        xor_cache_line(acc8 + i, src8 + i);
+                }
+
+                if (__builtin_expect(i >= n_bytes, 1)) {
+                        return;
+                }
+        } else if (__builtin_expect(n_bytes == 0, 0)) {
+                /* Help GCC 5 codegen just a little with edge probabilities. */
+                return;
         }
 
         for (; i < n_bytes; i++) {
